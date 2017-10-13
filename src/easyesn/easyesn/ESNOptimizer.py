5370d063f861b1e7a47c712839120c3b44264465
FlashTek <rzrolandzimmermann@gmail.com>
2017-10-13 13:37:47 +0200

    fix: small refactoring and renaming

--- src/easyesn/easyesn/ESNOptimizer.py
+++ src/easyesn/easyesn/ESNOptimizer.py
@@ -73,7 +73,7 @@ class ESNOptimizer(object):
     ####################################################################################################################################################
 
 
-    def optimizeParameterForTrainError(self, trainInputs, trainTargets, validationInputs, validationTargets,
+    def optimizeParameterForTrainError(self, trainingInputData, trainingOutputData, validationInputData, validationOutputData,
                                        learningRate=0.0001, epochs=1, transientTime=None, verbose=1):
 
         self._validateReservoir()
@@ -82,15 +82,21 @@ class ESNOptimizer(object):
             raise ValueError("Only penalty optimization is supported for a multiple input scalings at the moment. We are working on it.")
 
         # calculate stuff
-        trainLength = trainInputs.shape[0]
+        trainLength = trainingInputData.shape[0]
         if transientTime is None:
-            transientTime = self._reservoir.estimateTransientTime(trainInputs, trainTargets)
+            transientTime = self._reservoir.estimateTransientTime(trainingInputData, trainingOutputData)
 
         # initializations of arrays:
-        if (len(trainTargets.shape) == 1):
-            Ytarget = trainTargets[None, transientTime:].T
+        if (len(trainingOutputData.shape) == 1):
+            Ytarget = trainingOutputData[None, transientTime:].T
         else:
-            Ytarget = trainTargets[transientTime:].T
+            Ytarget = trainingOutputData[transientTime:].T
+
+        if (len(validationOutputData.shape) == 1):
+            validationOutputData = validationOutputData[:, None]
+
+        Ytarget = self._reservoir.out_inverse_activation(Ytarget)
+        validationOutputData = self._reservoir.out_inverse_activation(validationOutputData)
 
         # initializations for plotting parameter and losses at the end
         inputScalings = list()
@@ -115,7 +121,7 @@ class ESNOptimizer(object):
         oldIS = self._reservoir._inputScaling
 
         # initialize self.designMatrix and self.W_out
-        oldLoss = self._reservoir.fit(trainInputs, trainTargets, transientTime=transientTime)
+        oldLoss = self._reservoir.fit(trainingInputData, trainingOutputData, transientTime=transientTime)
 
         # Calculate uniform matrices
         W_uniform = self._reservoir._W / self._reservoir._spectralRadius
@@ -138,7 +144,7 @@ class ESNOptimizer(object):
             x = B.zeros((self._reservoir.n_reservoir, 1))
 
             # go thorugh the train length (e.g. the time, on which W_out gets calculated)
-            for t, u in enumerate(trainInputs):
+            for t, u in enumerate(trainingInputData):
                 u = u.reshape(-1, 1)
                 oldx = x
                 self._reservoir.update(u)
@@ -171,14 +177,14 @@ class ESNOptimizer(object):
             x = B.zeros((self._reservoir.n_reservoir, 1))
 
             # go through the train time again, and this time, calculate del error / del (rho, alpha, s_in) based on del W_out and the single derivatives
-            for t, u in enumerate(trainInputs):
+            for t, u in enumerate(trainingInputData):
                 u = u.reshape(-1, 1)
                 self._reservoir.update(u)
                 x = self._reservoir._x
                 if t >= transientTime:
 
                     # calculate error at given time step
-                    error = (trainTargets[t] - B.dot( self._reservoir._W_out, B.vstack((1, u, x)) ) ).T
+                    error = (trainingOutputData[t] - B.dot( self._reservoir._W_out, B.vstack((1, u, x)) ) ).T
 
                     # calculate gradients
                     gradientSR = B.dot(-error, B.dot(WoutPrimeSR, B.vstack((1, u, x))[:, 0]) + B.dot(self._reservoir._W_out, srGradientsMatrix[:, t - transientTime]))
@@ -210,8 +216,8 @@ class ESNOptimizer(object):
             self._reservoir.setInputScaling(self._reservoir._inputScaling - learningRate * gradientIS)
 
             # calculate the errors and update the self.designMatrix and the W_out
-            fitLoss = self._reservoir.fit(trainInputs, trainTargets, transientTime=transientTime)
-            validationLoss = hlp.loss( self._reservoir.predict(validationInputs), validationTargets )
+            fitLoss = self._reservoir.fit(trainingInputData, trainingOutputData, transientTime=transientTime)
+            validationLoss = hlp.loss( self._reservoir.predict(validationInputData), validationOutputData )
 
             if fitLoss > oldLoss:
                 self._reservoir.setSpectralRadius(oldSR)
@@ -234,7 +240,7 @@ class ESNOptimizer(object):
 
         return (validationLosses, fitLosses, inputScalings, leakingRates, spectralRadiuses)
 
-    def optimizeParameterForEvaluationError(self, trainInputs, trainTargets, validationInputs, validationTargets,
+    def optimizeParameterForEvaluationError(self, trainingInputData, trainingOutputData, validationInputData, validationOutputData,
                                             optimizationLength, learningRate=0.0001, epochs=1, transientTime=None, verbose=1):
 
         self._validateReservoir()
@@ -243,15 +249,21 @@ class ESNOptimizer(object):
             raise ValueError("Only penalty optimization is supported for a multiple input scalings at the moment. We are working on it.")
 
         # calculate stuff
-        trainLength = trainInputs.shape[0]
+        trainLength = trainingInputData.shape[0]
         if transientTime is None:
-            transientTime = self._reservoir.estimateTransientTime(trainInputs, trainTargets)
+            transientTime = self._reservoir.estimateTransientTime(trainingInputData, trainingOutputData)
 
         # initializations of arrays:
-        if (len(trainTargets.shape) == 1):
-            Ytarget = trainTargets[None, transientTime:].T
+        if (len(trainingOutputData.shape) == 1):
+            Ytarget = trainingOutputData[None, transientTime:].T
         else:
-            Ytarget = trainTargets[transientTime:].T
+            Ytarget = trainingOutputData[transientTime:].T
+
+        if (len(validationOutputData.shape) == 1):
+            validationOutputData = validationOutputData[:, None]
+
+        Ytarget = self._reservoir.out_inverse_activation(Ytarget)
+        validationOutputData = self._reservoir.out_inverse_activation(validationOutputData)
 
         # initializations for plotting parameter and losses at the end
         inputScalings = list()
@@ -276,8 +288,8 @@ class ESNOptimizer(object):
         oldIS = self._reservoir._inputScaling
 
         # initialize self.designMatrix and self.W_out
-        self._reservoir.fit(trainInputs, trainTargets, transientTime=transientTime)
-        oldLoss = hlp.loss( self._reservoir.predict(validationInputs), validationTargets )
+        self._reservoir.fit(trainingInputData, trainingOutputData, transientTime=transientTime)
+        oldLoss = hlp.loss( self._reservoir.predict(validationInputData), validationOutputData )
 
         # Calculate uniform matrices
         W_uniform = self._reservoir._W / self._reservoir._spectralRadius
@@ -298,7 +310,7 @@ class ESNOptimizer(object):
             x = B.zeros((self._reservoir.n_reservoir, 1))
 
             # go thorugh the train length (e.g. the time, on which W_out gets calculated)
-            for t, u in enumerate(trainInputs):
+            for t, u in enumerate(trainingInputData):
                 u = u.reshape(-1, 1)
                 oldx = x
                 x = self._reservoir._X[2:,t]
@@ -321,14 +333,14 @@ class ESNOptimizer(object):
             WoutPrimeIS = self.derivationWoutForP(Ytarget, self._reservoir._X, isGradientsMatrix)
 
             # this time go through validation length
-            for t, u in enumerate(validationInputs):
+            for t, u in enumerate(validationInputData):
                 u = u.reshape(-1, 1)
                 oldx = x
                 self._reservoir.update(u)
                 x = self._reservoir._x
 
                 # calculate error at given time step
-                error = (validationTargets[t] - B.dot( self._reservoir._W_out, B.vstack((1, u, x)) ) ).T
+                error = (validationOutputData[t] - B.dot( self._reservoir._W_out, B.vstack((1, u, x)) ) ).T
 
                 # calculate del x / del (rho, alpha, s_in)
                 derivationSpectralRadius = self.derivationForSpectralRadius(W_uniform, derivationSpectralRadius, u, oldx)
@@ -370,8 +382,8 @@ class ESNOptimizer(object):
             self._reservoir.setInputScaling(self._reservoir._inputScaling - learningRate * gradientIS)
 
             # calculate the errors and update the self.designMatrix and the W_out
-            fitLoss = self._reservoir.fit(trainInputs, trainTargets, transientTime=transientTime)
-            validationLoss = hlp.loss( self._reservoir.predict(validationInputs), validationTargets )
+            fitLoss = self._reservoir.fit(trainingInputData, trainingOutputData, transientTime=transientTime)
+            validationLoss = hlp.loss( self._reservoir.predict(validationInputData), validationOutputData )
 
             # this is the "when the error goes up, go back and divide learning rate by 2" mechanism
             if validationLoss > oldLoss:
@@ -397,18 +409,22 @@ class ESNOptimizer(object):
 
         return (validationLosses, fitLosses, inputScalings, leakingRates, spectralRadiuses)
 
-    def optimizePenaltyForEvaluationError(self, trainInputs, trainTargets, validationInputs, validationTargets,
+    def optimizePenaltyForEvaluationError(self, trainingInputData, trainingOutputData, validationInputData, validationOutputData,
                                           optimizationLength, learningRate=0.0001, epochs=1, penalty=0.1, transientTime=0, verbose=1):
 
         self._validateReservoir()
 
-        if (len(trainTargets.shape) == 1):
-            Ytarget = trainTargets[None, transientTime:].T
+        # initializations of arrays:
+        if (len(trainingOutputData.shape) == 1):
+            Ytarget = trainingOutputData[None, transientTime:].T
         else:
-            Ytarget = trainTargets[transientTime:].T
+            Ytarget = trainingOutputData[transientTime:].T
 
-        #TODO solve this...
-        penalty = penalty
+        if (len(validationOutputData.shape) == 1):
+            validationOutputData = validationOutputData[:, None]
+
+        Ytarget = self._reservoir.out_inverse_activation(Ytarget)
+        validationOutputData = self._reservoir.out_inverse_activation(validationOutputData)
 
         fitLosses = list()
         validationLosses = list()
@@ -417,13 +433,13 @@ class ESNOptimizer(object):
         penaltyDerivatives = B.zeros(optimizationLength)
         oldPenalty = penalty
 
-        self._reservoir.fit(trainInputs, trainTargets, transientTime=transientTime)
-        oldLoss = hlp.loss(self._reservoir.predict(validationInputs), validationTargets)
+        self._reservoir.fit(trainingInputData, trainingOutputData, transientTime=transientTime)
+        oldLoss = hlp.loss(self._reservoir.predict(validationInputData), validationOutputData)
 
         evaluationEchoFunction = B.zeros((1 + self._reservoir.n_reservoir + self._reservoir.n_input, optimizationLength))
         x = self._reservoir.x
 
-        for t, u in enumerate(validationInputs):
+        for t, u in enumerate(validationInputData):
             u = u.reshape(-1, 1)
             self._reservoir.update(u)
             x = self._reservoir._x
@@ -439,9 +455,9 @@ class ESNOptimizer(object):
 
             penaltyDerivative = self.derivationForPenalty(Ytarget, self._reservoir._X, penalty)
 
-            for t in range(len(validationInputs)):
+            for t in range(len(validationInputData)):
                 predictionPoint = B.dot(self._reservoir._W_out, evaluationEchoFunction[:, t].reshape(-1, 1))
-                error = (trainTargets[t] - predictionPoint).T
+                error = (trainingOutputData[t] - predictionPoint).T
                 penaltyDerivatives[t] = - B.dot(B.dot(error, penaltyDerivative), predictionPoint)
 
             penaltyGradient = sum(penaltyDerivatives)
@@ -452,8 +468,8 @@ class ESNOptimizer(object):
 
             self._reservoir.setPenalty(penalty)
             self._reservoir._calculateOutputMatrix()
-            fitLoss = hlp.loss( self._reservoir.predict( trainInputs), trainTargets )
-            validationLoss = hlp.loss( self._reservoir.predict(validationInputs), validationTargets )
+            fitLoss = hlp.loss( self._reservoir.predict( trainingInputData), trainingOutputData )
+            validationLoss = hlp.loss( self._reservoir.predict(validationInputData), validationOutputData )
 
             # this is the "when the error goes up, go back and divide learning rate by 2" mechanism
             if validationLoss > oldLoss:
